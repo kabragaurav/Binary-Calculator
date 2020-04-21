a4a554fed7672636fbfeee942fff898fb474dbd3
Gaurav Kabra <gauravkabra12@gmail.com>
2020-04-21 14:24:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Author : [Gaurav Kabra](https://www.quora.com/profile/Gaurav-Kabra-23)
+## About
+This is binary calculator, which takes two binary numbers as input and displays the output in binary form.
+<br>
+##### Note:
+It is quite robust as it can detect invalid expressions like "1--1"<br> or invalid mathematical expressions like "1/0".
+<br>
+If you still find some functionality that is not working then create a pull request.
---
+++ js/binaryCalculator.js
@@ -0,0 +1,110 @@
+var expr="";
+
+function addZero(){
+    expr=expr+"0";
+    document.getElementById("res").innerHTML = expr;
+}
+
+function addOne(){
+    expr=expr+"1";
+    document.getElementById("res").innerHTML = expr;
+}
+
+function binToDec(s) {
+  var result = 0;
+  var len = s.length - 1;
+  for (var i = len; i >= 0; i--)
+    result += s[i] * Math.pow(2, len-i);  
+  return result;
+}
+
+function decToBin(n) {
+  if(n<=0)
+  		return "0";
+  var result = '';
+  // find highest integer power of 2 equal or less then n
+  var order = Math.floor(Math.log2(n));
+  while (order >= 0) {
+    var k = Math.pow(2, order);
+    // if n >= 2^order - reduce that value from n and add 1 to result
+    if (n >= k) {
+      n = n - k;
+      result += 1;
+    } else {
+    // else add 0
+      result += 0;
+    }
+    // reduce order by 1
+    order = order - 1;
+  }
+  return result;
+}
+
+function reset(){
+    expr="";
+    document.getElementById("res").innerHTML = expr;
+}
+function evaluateIt(){
+    let result=0;
+    try{
+      if(expr.includes("+"))
+          arr=expr.split('+');
+      if(expr.includes("-"))
+          arr=expr.split('-');
+      if(expr.includes("*"))
+          arr=expr.split('*');
+      if(expr.includes("/"))
+          arr=expr.split('/');
+      // arr should have exactly 2 strings
+      //E.g. if 1--1 is entered
+      // then arr=["1","","1"] (you can check from Opera's Inspect Element's Console window)
+      if(arr.length!=2)	
+      	throw "Invalid expression";
+      console.log(arr[0]);
+      console.log(arr[1]);
+      dec1 = binToDec(arr[0]);
+      dec2 = binToDec(arr[1]);
+      console.log(dec1);
+      console.log(dec2);
+      if(expr.includes("+"))
+          result=dec1+dec2;
+      if(expr.includes("-"))
+          result=dec1-dec2;
+      if(expr.includes("*"))
+          result=dec1*dec2;
+      if(expr.includes("/"))
+      {
+      	if(dec2==0)
+      		throw "Denominator cannot be 0";
+        result=dec1/dec2;
+      }
+      //convert back to string
+      expr=""+decToBin(result);
+      document.getElementById("res").innerHTML = expr;
+    }
+    catch(e){
+      expr="";
+      document.getElementById("res").innerHTML=expr;
+      alert(e);
+    }
+}
+
+function addPlus(){
+    expr=expr+"+";
+    document.getElementById("res").innerHTML = expr;
+}
+
+function addMinus(){
+    expr=expr+"-";
+    document.getElementById("res").innerHTML = expr;
+}
+
+function addMul(){
+    expr=expr+"*";
+    document.getElementById("res").innerHTML = expr;
+}
+
+function addDiv(){
+    expr=expr+"/";
+    document.getElementById("res").innerHTML = expr;
+}
